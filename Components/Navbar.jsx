453ddf51f80f62ed806234764e42b5945c9756dd
juliuskon <someoneforsale123@gmail.com>
2025-09-05 09:14:47 +0800

    nara nya

---
+++ Components/Navbar.jsx
@@ -0,0 +1,9 @@
+"use client"; // only if it uses hooks (useState, useEffect, etc.)
+
+export default function Navbar() {
+  return (
+    <nav className="p-4 bg-gray-800 text-white">
+      <h1>My Navbar</h1>
+    </nav>
+  );
+}
